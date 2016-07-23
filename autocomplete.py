03f99b3f01c4bc0cdb947820a676efe30e6dea3a
zzh1996 <zzh1996@mail.ustc.edu.cn>
2016-07-24 02:49:57 +0800

    threaded request

--- autocomplete.py
+++ autocomplete.py
@@ -7,14 +7,14 @@ import requests, json, sys
 def google(query, pos=-1):
     url = 'https://www.google.com/complete/search'
     payload = {
-        #'sclient': 'psy-ab',
+        # 'sclient': 'psy-ab',
         'gs_ri': 'psy-ab',
         'q': query,
         'tch': 1,
         'ech': '',
         'cp': len(query) if pos < 0 else pos
     }
-    r = requests.get(url, params=payload)
+    r = requests.get(url, params=payload, timeout=5)
     j = json.loads(r.text[:-6])
     d = json.loads(j['d'])
     return [i[0] for i in d[1]]
--- main.py
+++ main.py
@@ -3,6 +3,40 @@
 
 from curses import *
 from autocomplete import *
+import threading
+
+thread_cnt = 0
+choices = []
+choice = 0
+choice_cnt = 0
+lock = threading.Lock()
+
+
+class RequestThread(threading.Thread):
+    def __init__(self, text, pos, cnt, stdscr):
+        threading.Thread.__init__(self)
+        self.text = text
+        self.pos = pos
+        self.cnt = cnt
+        self.stdscr = stdscr
+
+    def run(self):
+        global lock, choice, choice_cnt, choices, thread_cnt
+        r = google(self.text, self.pos)
+        if thread_cnt == self.cnt:
+            lock.acquire()
+            choices.clear()
+            for i in r:
+                if i.replace('<b>', '').replace('</b>', '') != self.text:
+                    choices.append(i)
+            choice_cnt = len(choices)
+            choice = 0
+            for i, c in enumerate(choices):
+                self.stdscr.move(i + 1, 0)
+                render_choice(self.stdscr, c, i == choice)
+            self.stdscr.move(0, self.pos)
+            self.stdscr.refresh()
+            lock.release()
 
 
 def render_choice(stdscr, c, selected):
@@ -20,22 +54,24 @@ def render_choice(stdscr, c, selected):
 
 
 def main(stdscr):
+    global lock, choice, choice_cnt, choices, thread_cnt
     stdscr.clear()
     text = ''
     pos = 0
-    choice = 0
-    choice_cnt = 0
     change = False
-    choices = []
     while True:
         k = stdscr.getch()
         change = False
         if k == KEY_DOWN:
+            lock.acquire()
             if choice < choice_cnt - 1:
                 choice += 1
+            lock.release()
         elif k == KEY_UP:
+            lock.acquire()
             if choice > 0:
                 choice -= 1
+            lock.release()
         elif k == KEY_LEFT:
             if pos > 0:
                 pos -= 1
@@ -43,10 +79,12 @@ def main(stdscr):
             if pos < len(text):
                 pos += 1
         elif k == ord('\t'):
+            lock.acquire()
             if choice_cnt > 0:
-                text = choices[choice].replace('<b>','').replace('</b>','')
-                pos=len(text)
+                text = choices[choice].replace('<b>', '').replace('</b>', '')
+                pos = len(text)
                 change = True
+            lock.release()
         elif k == ord('\n'):
             return
         elif k == KEY_BACKSPACE:
@@ -59,19 +97,28 @@ def main(stdscr):
             pos += 1
             change = True
 
+        lock.acquire()
         stdscr.clear()
         stdscr.addstr(0, 0, text)
         stdscr.move(0, pos)
         stdscr.refresh()
+        lock.release()
         if change:
-            choices = google(text, pos)
+            lock.acquire()
             choice = 0
-            choice_cnt = len(choices)
+            choice_cnt = 0
+            choices = []
+            lock.release()
+            thread_cnt += 1
+            thread = RequestThread(text, pos, thread_cnt, stdscr)
+            thread.start()
+        lock.acquire()
         for i, c in enumerate(choices):
             stdscr.move(i + 1, 0)
             render_choice(stdscr, c, i == choice)
         stdscr.move(0, pos)
         stdscr.refresh()
+        lock.release()
 
 
 wrapper(main)
